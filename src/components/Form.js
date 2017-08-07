d1a8a693674b4c01c73224e30e63d8b53dfa6d3b
Gale Bonker <gbonker@andrew.cmu.edu>
2017-08-06 18:38:29 -0700

    add accessibility features and finalize responsiveness

--- src/components/Form.js
+++ src/components/Form.js
@@ -176,11 +176,14 @@ class Form extends Component {
               name="businessName" 
               type="text" 
               className="form-control input-lg form-input" 
-              aria-describedby="businessNameHelp" 
+              aria-describedby="business-name-help" 
               value={this.state.businessName} 
               onChange={event => this.change(event)}
             />
             <FormError formError={this.state.formErrors.businessName} />
+            <div id="business-name-help" className="offscreen">
+              Business Name. Enter the name of your business.
+            </div>
           </div>
           
           <div className={`form-group ${this.errorClass(this.state.formErrors.businessEmail)}`}>
@@ -189,11 +192,14 @@ class Form extends Component {
               name="businessEmail"
               type="email" 
               className="form-control input-lg form-input" 
-              aria-describedby="businessEmailHelp" 
+              aria-describedby="business-email-help" 
               value={this.state.businessEmail} 
               onChange={event => this.change(event)}
             />
             <FormError formError={this.state.formErrors.businessEmail} />
+            <div id="business-name-help" className="offscreen">
+              Business Email. Enter your business email, where a confirmation email will be sent.
+            </div>
           </div>
           <div className={`form-group ${this.errorClass(this.state.formErrors.username)}`}>
             <label className="form-label">Create a Username</label>
@@ -201,11 +207,14 @@ class Form extends Component {
               name="username"
               type="text" 
               className="form-control input-lg form-input" 
-              aria-describedby="usernameHelp" 
+              aria-describedby="username-help" 
               value={this.state.username} 
               onChange={event => this.change(event)}
             />
             <FormError formError={this.state.formErrors.username} />
+            <div id="username-help" className="offscreen">
+              Username. Create a username to be used to log into your new account.
+            </div>
           </div>
           <div className={`form-group ${this.errorClass(this.state.formErrors.password)}`}>
             <label className="form-label">Password <span className="text-muted"><small>6 characters | 1 uppercase | 1 lowercase | 1 digit</small></span></label>
@@ -213,11 +222,14 @@ class Form extends Component {
               name="password"
               type="password" 
               className="form-control input-lg form-input" 
-              aria-describedby="passwordHelp" 
+              aria-describedby="password-help" 
               value={this.state.password} 
               onChange={event => this.change(event)}
             />
             <FormError formError={this.state.formErrors.password} />
+            <div id="password-help" className="offscreen">
+              Password. Pick a password for your new account. Your password must contain at least six characters, including at least one lowercase letter, one uppercase letter, and one digit.
+            </div>
           </div>
           <div className="form-group">
             <label className="form-label">Website <span className="text-muted"><small>(Optional)</small></span></label>
@@ -226,28 +238,34 @@ class Form extends Component {
               type="text"
               placeholder="mywebsite.com" 
               className="form-control input-lg form-input" 
-              aria-describedby="websiteHelp" 
+              aria-describedby="website-help" 
               value={this.state.website} 
               onChange={event => this.change(event)}
             />
+            <div id="website-help" className="offscreen">
+              Website. Enter your business's website. This field is optional.
+            </div>
           </div>
           <div className={`form-group ${this.errorClass(this.state.formErrors.typeOfBusiness)}`}>
             <label className="form-label">Type of Business</label>
             <select 
               name="typeOfBusiness"
               className="form-control input-lg form-input"
-              aria-describedby="typeOfBusinessHelp"
+              aria-describedby="type-of-business-help"
               value={this.state.typeOfBusiness} 
               onChange={event => this.change(event)}
               >
               <option value="">Select your business</option>
-              <option value="restaurant">Restaurant</option>
               <option value="ecommerce">Ecommerce</option>
+              <option value="restaurant">Restaurant</option>
               <option value="retail">Retail</option>
               <option value="telecommunications">Telecommunications</option>
               <option value="other">Other</option>
             </select>
             <FormError formError={this.state.formErrors.typeOfBusiness} />
+            <div id="type-of-business-help" className="offscreen">
+              Type of Business. Please select what category your business falls in. If none of the categories describe your business well, select "Other."
+            </div>
           </div>
           <div className="">
             <p><strong>Terms of Service</strong></p>
@@ -255,7 +273,7 @@ class Form extends Component {
               <input 
                 name="termsOfService"
                 type="checkbox" 
-                aria-describedby="termsOfServiceHelp"
+                aria-describedby="terms-of-service-help"
                 className="" 
                 checked={this.state.termsOfService} 
                 onChange={event => this.changeCheckbox(event)}
@@ -266,6 +284,9 @@ class Form extends Component {
                 </span>
             </label>
             <FormError formError={this.state.formErrors.termsOfService} />
+            <div id="terms-of-service-help" className="offscreen">
+              Terms of Service. You must agree to the terms of service to continue. Click on the "terms of service" link for details.
+            </div>
           </div>
           <div className="">
             <p><strong>Privacy Policy</strong></p>
@@ -273,7 +294,7 @@ class Form extends Component {
               <input 
                 name="privacyPolicy"
                 type="checkbox"
-                aria-describedby="privacyPolicyHelp" 
+                aria-describedby="privacy-policy-help" 
                 className="" 
                 checked={this.state.privacyPolicy} 
                 onChange={event => this.changeCheckbox(event)}
@@ -284,13 +305,20 @@ class Form extends Component {
                 </span>
             </label>
             <FormError formError={this.state.formErrors.privacyPolicy} />
+            <div id="privacy-policy-help" className="offscreen">
+              Privacy Policy. You must agree to the privacy policy to continue. Click on the "privacy policy" link for details.
+            </div>
           </div>
           <button 
             onClick={(event) => this.onSubmit(event)} 
-            className="btn btn-primary btn-lg center-block register-button" 
+            className="btn btn-primary btn-lg center-block register-button"
+            aria-describedby="register-button-help" 
             disabled={!this.state.formValid}>
               Register
           </button>
+          <div id="privacy-policy-help" className="offscreen">
+            Click this button to register. If this button is disabled, you have not completed all the fields correctly. If this is the case, go back and check your answers to the form questions. Most importantly, make sure you have entered a valid email and password.
+          </div>
         </form>
       </div>
     );
